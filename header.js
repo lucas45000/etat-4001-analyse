8239bf07fd00eb52848a719c16c34e3776bb6e3c
lucas45000 <lucas.balland@sciencespo.fr>
2025-11-11 11:16:17 +0100

    Create header.js

---
+++ header.js
@@ -0,0 +1,12 @@
+let lastScrollTop = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop && scrollTop > 100) {
+    header.classList.add('hidden');
+  } else {
+    header.classList.remove('hidden');
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
